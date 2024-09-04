727ad4066ad8dc12996e5410262f52041cf0dad7
hameed <hameedse2943@gmail.com>
2024-09-04 12:51:08 +0500

    Add Initial Files

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeDetails() {
+    document.getElementById("studentName").textContent = "Jane Doe";
+    document.getElementById("studentAge").textContent = "Age: 21";
+    document.getElementById("studentGender").textContent = "Gender: Female";
+    document.getElementById("studentUniversity").textContent = "University: ABC University";
+    document.getElementById("studentEmail").textContent = "Email: janedoe@example.com";
+    document.getElementById("studentPhoto").src = "new-photo.jpg";
+}
